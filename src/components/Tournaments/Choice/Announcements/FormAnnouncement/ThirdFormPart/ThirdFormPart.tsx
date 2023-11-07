17988dcb708bc9abded6694f900df394c87743f7
Artem <yisssssssss@gmail.com>
2023-11-07 15:01:21 +0300

    Changing styles + update form

--- src/components/Tournaments/Choice/Announcements/FormAnnouncement/ThirdFormPart/ThirdFormPart.tsx
+++ src/components/Tournaments/Choice/Announcements/FormAnnouncement/ThirdFormPart/ThirdFormPart.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import stilesTFP from "./ThirdFormPart.module.css"
 import {Button, Input, InputNumber, Select, Upload, UploadProps, App} from "antd";
 import {PhoneOutlined, UploadOutlined, UserOutlined} from "@ant-design/icons";
+import TextArea from "antd/es/input/TextArea";
 
 
 export const ThirdFormPart = () => {
 
     const {message} = App.useApp();
+
+    const onChangeTextAria = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        console.log('Change:', e.target.value);
+    };
 
     const props: UploadProps = {
         name: 'file',
@@ -52,20 +57,21 @@ export const ThirdFormPart = () => {
         <>
             <div className={stilesTFP.thirdForm__contain}>
                 <div className={stilesTFP.financeInput__contain}>
-                    <InputNumber addonAfter={selectAfter} style={{width: 200}} placeholder={'Стартовый взнос'}/>
-                    <InputNumber addonAfter={selectAfter} style={{width: 200}} placeholder={'Призовой фонд'}/>
+                    <InputNumber addonAfter={selectAfter} style={{width: 150}} placeholder={'Взнос'}/>
+                    <InputNumber addonAfter={selectAfter} style={{width: 180, fontSize:'10px'}} placeholder={'Призовой фонд'}/>
                 </div>
                 <div className={stilesTFP.organizer__contain}>
-                    <Input placeholder="Организатор" prefix={<UserOutlined/>}/>
-                    <Input style={{width: "200px"}} type={'number'} placeholder="Телефон" prefix={<PhoneOutlined/>}/>
+                    <Input style={{width: "200px"}} placeholder="Организатор" prefix={<UserOutlined/>}/>
+                    <Input style={{width: "130px"}} type={'number'} placeholder="Телефон" prefix={<PhoneOutlined/>}/>
                 </div>
-                <Input addonBefore="http://" placeholder={"Ссылка на соц. сети"}/>
+                <Input style={{width: "340px"}} addonBefore="http://" placeholder={"Ссылка на соц. сети"}/>
+                <TextArea showCount maxLength={150} onChange={onChangeTextAria} placeholder="Дополнительные сведения" />
                 <div className={stilesTFP.upload__contain}>
                     <Upload {...props}>
-                        <Button icon={<UploadOutlined/>}>Загрузить афишу</Button>
+                        <Button icon={<UploadOutlined/>}>Добавить афишу</Button>
                     </Upload>
                     <Upload {...props}>
-                        <Button icon={<UploadOutlined/>}>Загрузить положение</Button>
+                        <Button icon={<UploadOutlined/>}>Добавить положение</Button>
                     </Upload>
                 </div>
 
